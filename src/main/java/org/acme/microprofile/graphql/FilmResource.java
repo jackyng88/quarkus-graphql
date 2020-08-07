5de8f5e67c46b1edc457a20a0f2935ac5269812f
JACKY NG <jacky.ng@ibm.com>
2020-08-07 14:41:20 -0400

    First Commit

---
+++ src/main/java/org/acme/microprofile/graphql/FilmResource.java
@@ -0,0 +1,57 @@
+package org.acme.microprofile.graphql;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.graphql.DefaultValue;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
+
+@GraphQLApi 
+public class FilmResource {
+
+    @Inject
+    GalaxyService service;
+
+    @Query("allFilms") 
+    @Description("Get all Films from a galaxy far far away") 
+    public List<Film> getAllFilms() {
+        return service.getAllFilms();
+    }
+
+    @Query
+    @Description("Get a Films from a galaxy far far away")
+    public Film getFilm(@Name("filmId") int id) {
+        return service.getFilm(id);
+    }
+
+    public List<Hero> heroes(@Source Film film) { 
+        return service.getHeroesByFilm(film);
+    }
+
+    @Mutation
+    public Hero createHero(Hero hero) {
+        service.addHero(hero);
+        return hero;
+    }
+
+    @Mutation
+    public Hero deleteHero(int id) {
+        return service.deleteHero(id);
+    }
+
+    @Query
+    public List<Hero> getHeroesWithSurname(@DefaultValue("Skywalker") String surname) {
+        return service.getHeroesBySurname(surname);
+    }
+
+    // @Query
+    // public Film getFilmsWithTitle(@DefaultValue("A New Hope") String title) {
+    //     return service.getFilmsByTitle(title);
+    // }
+}
